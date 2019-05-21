3112665e1057730348f1e685e73f7fdb54aea16b
Petri Autero <petri@gruntwork.io>
2019-05-21 08:35:33 +0300

    Remove defaults

--- examples/internal-load-balancer/variables.tf
+++ examples/internal-load-balancer/variables.tf
@@ -5,17 +5,14 @@
 
 variable "project" {
   description = "The project ID to create the resources in."
-  default     = "dev-sandbox-228703"
 }
 
 variable "region" {
   description = "The region to create the resources in."
-  default     = "europe-north1"
 }
 
 variable "zone" {
   description = "The availability zone to create the sample compute instances in. Must within the region specified in 'var.region'"
-  default     = "europe-north1-a"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
