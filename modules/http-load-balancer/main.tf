21e6756532cf8e07cc2d9123ae2eeb77f5969112
Rob Morgan <robbym@gmail.com>
2020-11-12 15:24:58 +0800

    fix comment blocks

--- modules/http-load-balancer/main.tf
+++ modules/http-load-balancer/main.tf
@@ -4,7 +4,6 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 terraform {
-  # This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
   # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
   # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
   # forwards compatible with 0.13.x code.
@@ -89,4 +88,3 @@ resource "google_dns_record_set" "dns" {
 
   rrdatas = [google_compute_global_address.default.address]
 }
-
